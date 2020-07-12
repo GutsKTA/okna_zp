713727446e270ae9ed403c802dc1f5ca517afed0
Oleksii <lyosha.mak@gmail.com>
2020-07-12 21:04:16 +0300

    3th-commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@ window.addEventListener("scroll", function () {
   if (window.scrollY > 150) {
     document.querySelector("#main-nav").style.opacity = 0.9;
   } else {
-    document.querySelector("#main-nav").style.opacity = 0;
+    document.querySelector("#main-nav").style.opacity = 1;
   }
 });
 
