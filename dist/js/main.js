9f1bbe354a922fff52b0c6b8e435f09908ef151c
Oleksii <lyosha.mak@gmail.com>
2020-07-14 22:37:54 +0300

    commit 4th

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,24 +1,21 @@
 // Sticky menu background
-window.addEventListener("scroll", function () {
-  if (window.scrollY > 150) {
-    document.querySelector("#main-nav").style.opacity = 0.9;
+$(window).on("scroll", function () {
+  if ($(window).scrollTop()) {
+    $("nav").addClass("main-nav-white");
   } else {
-    document.querySelector("#main-nav").style.opacity = 1;
+    $("nav").removeClass("main-nav-white");
   }
 });
 
 // Smooth Scrolling
-$("#navbar a, .btn").on("click", function (event) {
+$("#main-nav a, .btn").on("click", function (event) {
   if (this.hash !== "") {
     event.preventDefault();
 
     const hash = this.hash;
 
-    $("html, body").animate(
-      {
-        scrollTop: $(hash).offset().top - 100,
-      },
-      800
-    );
+    $("html, body").animate({
+      scrollTop: $(hash).offset().top - 82,
+    });
   }
 });
